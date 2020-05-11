550ef33c3eab66413111f826bd9de79b90847d98
andrijaster <39303699+andrijaster@users.noreply.github.com>
2020-05-11 19:49:43 +0200

    AP

--- Pytorch/model/FCN_class_GQ_full.py
+++ Pytorch/model/FCN_class_GQ_full.py
@@ -8,31 +8,33 @@ Created on Sun Dec 15 18:45:27 2019
 import torch
 import numpy as np
 from torch import nn, optim
+from scipy.special import p_roots
 import random
 
 
 class FCN_point_process():
     
-    def __init__(self, in_size, out_size, drop=0, num_layers = 2,
+    def __init__(self, in_size, out_size = 1, drop=0, num_layers = 2,
                  step = 2):
         self.model = FCN_point_process.Model(in_size, out_size, 
                            dropout = drop,
                            num_layers = num_layers, step = step)
-        
-    def Gaussian_quadrature(vec, lower_l, upper_l):
-        vec = vec.reshape(-1)
+  
+    def Gaussian_quadrature(self, n, lower_l, upper_l):
         m = (upper_l-lower_l)/2
         c = (upper_l+lower_l)/2
-        vec = (vec-c)/m
-        A = np.array([vec**i for i in range(0,vec.shape[0])])
-        B = np.array([(1**i-(-1)**i)/i for i in range(1,vec.shape[0]+1)])
-        x = m*np.linalg.solve(A, B)
-        return x
+        [x,w] = p_roots(n+1)
+        self.weights = w*m
+        # self.weights = torch.tensor(self.weights).type('torch.FloatTensor') 
+        self.time_train_integral = m*x+c
+        return [self.time_train_integral, self.weights]
     
-    def loss(lam, weights):
+    def loss(lam, targets, weights):
         ML=0
         for i in range(len(lam)):
-            ML += torch.sum(torch.log(lam[i])) - torch.sum(weights[i]*lam[i])
+            y1 = torch.log(lam[i]) * targets[:,i].reshape(-1,1)
+            y2 = weights*lam[i][torch.where(targets[:,i]==0)]
+            ML += torch.sum(y1) - torch.sum(y2)
         return torch.neg(ML)
 
     class Model(nn.Module):
@@ -63,37 +65,28 @@ class FCN_point_process():
                 out.append(torch.exp(self.fc(x[i])))
             return out
      
-    def fit(self, x_train, time, no_epoch = 100, log = 1, log_epoch = 5, 
+    def fit(self, x_train, time, targets, weights, no_epoch = 100, log = 1, log_epoch = 5, 
             mini_batch_size = 10):
         
         " Training "
         self.model.train()
-        
+    
+        targets = 1-torch. transpose(targets,0,1)
         list_1 = list(self.model.parameters())
         optimizer_1 = torch.optim.Adam(list_1, lr = 0.001)
-        
-        self.weights = []
-        if time.shape[0]>1:
-            for i in range(time.shape[0]):
-                weights = FCN_point_process.Gaussian_quadrature(time[i], lower_l = 0, upper_l = time[i,-1])
-                self.weights.append(torch.tensor(weights))
-        else:
-            weights = FCN_point_process.Gaussian_quadrature(time, lower_l = 0, upper_l = time[-1])
-            self.weights = torch.tensor(weights)
         
         for e in range(no_epoch):
             for i in range(0, x_train.shape[0], mini_batch_size):     
                 batch_x = x_train[i:i+mini_batch_size]
-                weights = self.weights[i:i+mini_batch_size]
                 lambda_par = self.model(batch_x)
-                loss1 = FCN_point_process.loss(lambda_par, weights)
+                loss1 = FCN_point_process.loss(lambda_par, targets, weights)
                 optimizer_1.zero_grad()
                 loss1.backward()
                 optimizer_1.step()
         
             if e%log_epoch==0 and log == 1:
                 lambda_tot = self.model(x_train)
-                print(FCN_point_process.loss(lambda_tot, self.weights))
+                print(FCN_point_process.loss(lambda_tot, targets, self.weights))
                 
                 
     def predict(self, x_test):
@@ -105,13 +98,6 @@ class FCN_point_process():
         self.model.eval()
         output =self.model(x_test)
         weights_eval = []
-        if time_test.shape[0]>1:
-            for i in range(time_test.shape[0]):
-                weights = FCN_point_process.Gaussian_quadrature(time_test[i], lower_l = 0, upper_l = time_test[i,-1])
-                weights_eval.append(torch.tensor(weights))
-        else:
-            weights = FCN_point_process.Gaussian_quadrature(time_test[i], lower_l = 0, upper_l = time_test[-1])
-            weights_eval = torch.tensor(weights)
         NLL = FCN_point_process.loss(output, weights_eval)
         return NLL
     
@@ -122,23 +108,51 @@ if __name__ == "__main__":
     time = 100*np.random.rand(100)
     time.sort()
     in_size = 11
-    no_epoch = 10000
-    out_size = 1
-    # time_train = np.random.choice(time, size= 30, replace = False)
-    x = np.random.randn(2,len(time),in_size-1)
+    no_rows = 1
+    no_epoch = 30000
+    
+    x = np.random.randn(10,len(time),in_size-1)
     x = np.insert(x, 10, values = time, axis =2)
-    idx = random.sample(range(x.shape[1]), 50)
+    idx = random.sample(range(x.shape[1]), 15)
     idx.sort()
-    x_train = x[:,idx,:]
-    time_train = x[:,idx,-1]
-    time_test = x[:,:,-1]
+    x_train_occurance = x[:,idx,:]
+    time_train_occurance = x[:,idx,-1]
+    # time_test = x[:,:,-1]
     
     
-    x_train = torch.tensor(x_train).type('torch.FloatTensor')
-    x = torch.tensor(x).type('torch.FloatTensor')
+    # x_train = torch.tensor(x_train).type('torch.FloatTensor')
+    # x = torch.tensor(x).type('torch.FloatTensor')
     # time = torch.tensor(time).type('torch.FloatTensor').reshape(-1,1)
     
-    mod = FCN_point_process(in_size, out_size, drop = 0.0)
-    mod.fit(x_train, time_train, no_epoch = no_epoch)
-    print(mod.evaluate_NLL(x,time_test))
-    print(mod.predict(x))
+    mod = FCN_point_process(in_size, drop = 0.0)
+    [time_train_integral, weights] = mod.Gaussian_quadrature(n = 30,lower_l = 0, upper_l = 100)
+    weights = weights.reshape(-1,1)
+    time_train = []
+    targets = []
+    for i in range(no_rows):
+        val = np.random.choice(time, size= 30, replace = False)
+        val = np.insert(val, 0, time_train_integral, axis =0)
+        val.sort()
+        targets.append(np.isin(val,time_train_integral))
+        time_train.append(val)
+    
+    time_train = np.array(time_train)
+    targets = np.array(targets)*1
+    
+    # weight = np.zeros(targets.shape)
+    # for i in range(targets.shape[0]):
+    #     index = np.where(targets[i] == 1)
+    #     weight[i,index[0]] = weights
+            
+    x_train = np.random.randn(no_rows,time_train.shape[1],in_size-1)
+    x_train = np.insert(x_train, 10, values = time_train, axis =2)
+    
+    x_train = torch.tensor(x_train).type('torch.FloatTensor')
+    time_train = torch.tensor(time_train).type('torch.FloatTensor') 
+    targets = torch.tensor(targets).type('torch.FloatTensor')
+    weights = torch.tensor(weights).type('torch.FloatTensor')
+
+    
+    mod.fit(x_train, time_train, targets, weights, no_epoch = no_epoch)
+    # print(mod.evaluate_NLL(x,time_test))
+    print(mod.predict(x_train))
