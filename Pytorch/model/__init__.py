83f187be9b8155404907478f382aea82dd609cb2
andrijaster <39303699+andrijaster@users.noreply.github.com>
2020-05-22 13:25:15 +0200

    AP

--- Pytorch/model/__init__.py
+++ Pytorch/model/__init__.py
@@ -5,8 +5,7 @@ Created on Wed Dec 18 15:43:25 2019
 @author: Andri
 """
 
-from .RNN_class import RNN_point_process
-from .LSTM_class import LSTM_point_process
-from .GRU_class import GRU_point_process
-from .FCN_class import FCN_point_process
+from .RNN import RNN_point_process_all
+from .LSTM import LSTM_point_process_all
+from .GRU import GRU_point_process_all
 from .FCN import FCN_point_process_all
