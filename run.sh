3db5d778a32a0d9cec7ceca17d800c2c46a53954
DimitrijeM <dimitrijemilenkovic.dm@gmail.com>
2020-07-17 13:29:54 +0200

    flatten losses

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 export DATA_FOLDER="data/autoput/prepared/"
-export TRAINING_DATASET="stan1_traka1_012017.csv"
+export TRAINING_DATASET="stan1_traka1_01012017.csv"
 python3 src/auto_model_training.py
--- src/auto_model_training.py
+++ src/auto_model_training.py
@@ -12,6 +12,7 @@ if __name__ == "__main__":
     pd.set_option('display.max_rows', 500)
     pd.set_option('display.max_columns', 500)
     pd.set_option('display.width', 1000)
+    print(os.path.dirname(os.path.realpath(__file__)))
 
     data_folder = os.environ['DATA_FOLDER']
     data = pd.read_csv(data_folder+os.environ["TRAINING_DATASET"])
@@ -30,9 +31,9 @@ if __name__ == "__main__":
         {'rule': 'Simpsons', 'no_step': 10, 'learning_rate': 0.001},
         {'rule': 'Gaussian_Q', 'no_step': 10, 'learning_rate': 0.001}
     ]
-    analytical_definition = [{'rule': 'Analytical', 'no_step': 10, 'learning_rate': 0.001}]
+    analytical_definition = [{'rule': 'Analytical', 'no_step': 2, 'learning_rate': 0.001}]
     models_to_evaluate = [
-        {'model': FCN_point_process_all(in_size+1, out_size, drop=0.1), 'learning_param_map': learning_param_map},
+        # {'model': FCN_point_process_all(in_size+1, out_size, drop=0.1), 'learning_param_map': learning_param_map},
         {'model': GRU_point_process_all(in_size+1, out_size, drop=0.0), 'learning_param_map': learning_param_map},
         {'model': LSTM_point_process_all(in_size+1, out_size, drop=0.0), 'learning_param_map': learning_param_map},
         {'model': RNN_point_process_all(in_size+1, out_size, drop=0.0), 'learning_param_map': learning_param_map}
@@ -42,19 +43,20 @@ if __name__ == "__main__":
 
     in_size = 5
     out_size = 1
-    epochs = 50
+    no_epochs = 3
     evaluation_df = pd.DataFrame(columns=['model_name', 'rule', 'no_step', 'learning_rate', 'loss_on_train', 'loss_on_test'])
 
     for model_definition in models_to_evaluate:
         for params in model_definition['learning_param_map']:
             model = model_definition['model']
-            epochs, train_losses, test_losses = model.fit(train_time, test_time, in_size, no_epoch=epochs,
+            epochs, train_losses, test_losses = model.fit(train_time, test_time, in_size, no_epoch=no_epochs,
                       no_steps=params['no_step'], method=params['rule'], log_epoch=10)
 
             model_name = f"autoput-01012017-{type(model).__name__}-{params['rule']}"
+            train_losses = [loss.detach().numpy().flatten()[0] for loss in train_losses]
+            test_losses = [loss.detach().numpy().flatten()[0] for loss in test_losses]
+            print(train_losses, test_losses)
 
-            train_losses, test_losses = [loss.detach().numpy()[0, 0] for loss in train_losses], \
-                                        [loss.detach().numpy()[0, 0] for loss in test_losses]
             plt.plot(epochs, train_losses, color='skyblue', linewidth=2, label='train')
             plt.plot(epochs, test_losses, color='darkgreen', linewidth=2, linestyle='dashed', label="test")
             plt.legend()
@@ -62,19 +64,19 @@ if __name__ == "__main__":
 
             loss_on_train = model.evaluate(train_time, in_size, method='Trapezoid')
             loss_on_test = model.evaluate(test_time, in_size, method='Trapezoid')
-            print(f"Model: {type(model).__name__}. Loss on train: {str(loss_on_train.data.numpy())},  "
+            print(f"Model: {model_name}. Loss on train: {str(loss_on_train.data.numpy())},  "
                   f"loss on test: {str(loss_on_test.data.numpy())}")
             evaluation_df.loc[len(evaluation_df)] = [type(model).__name__,
                                                      params['rule'],
                                                      params['no_step'],
                                                      params['learning_rate'],
-                                                     loss_on_train.data.numpy()[0],
-                                                     loss_on_test.data.numpy()[0]]
-            model_filepath = f"../models/autoput-012017/{model_name}.torch"
+                                                     loss_on_train.data.numpy().flatten()[0],
+                                                     loss_on_test.data.numpy().flatten()[0]]
+            model_filepath = f"models/autoput-012017/{model_name}.torch"
             pickle.dump(model, open(model_filepath, 'wb'))
 
     print(evaluation_df)
-    evaluation_df.to_csv('../results/jan_autoput_scores_0.1.csv', index=False)
+    evaluation_df.to_csv('results/jan_autoput_scores_0.1.csv', index=False)
 
 
 
