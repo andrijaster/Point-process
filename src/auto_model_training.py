1f102aea7b6bff125898a6147ce215bd08271bec
DimitrijeM <dimitrijemilenkovic.dm@gmail.com>
2020-07-22 16:44:51 +0200

    results with lr=0.01

--- src/auto_model_training.py
+++ src/auto_model_training.py
@@ -25,15 +25,15 @@ if __name__ == "__main__":
     out_size = 1
 
     learning_param_map = [
-        {'rule': 'Euler', 'no_step': 10, 'learning_rate': 0.001},
-        {'rule': 'Implicit Euler', 'no_step': 10, 'learning_rate': 0.001},
-        {'rule': 'Trapezoid', 'no_step': 10, 'learning_rate': 0.001},
-        {'rule': 'Simpsons', 'no_step': 10, 'learning_rate': 0.001},
-        {'rule': 'Gaussian_Q', 'no_step': 10, 'learning_rate': 0.001}
+        {'rule': 'Euler', 'no_step': 6, 'learning_rate': 0.01},
+        {'rule': 'Implicit Euler', 'no_step': 6, 'learning_rate': 0.01},
+        {'rule': 'Trapezoid', 'no_step': 6, 'learning_rate': 0.01},
+        {'rule': 'Simpsons', 'no_step': 6, 'learning_rate': 0.01},
+        {'rule': 'Gaussian_Q', 'no_step': 6, 'learning_rate': 0.01}
     ]
     analytical_definition = [{'rule': 'Analytical', 'no_step': 2, 'learning_rate': 0.001}]
     models_to_evaluate = [
-        # {'model': FCN_point_process_all(in_size+1, out_size, drop=0.1), 'learning_param_map': learning_param_map},
+#        {'model': FCN_point_process_all(in_size+1, out_size, drop=0.1), 'learning_param_map': learning_param_map},
         {'model': GRU_point_process_all(in_size+1, out_size, drop=0.0), 'learning_param_map': learning_param_map},
         {'model': LSTM_point_process_all(in_size+1, out_size, drop=0.0), 'learning_param_map': learning_param_map},
         {'model': RNN_point_process_all(in_size+1, out_size, drop=0.0), 'learning_param_map': learning_param_map}
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
     in_size = 5
     out_size = 1
-    no_epochs = 80
+    no_epochs = 120
     evaluation_df = pd.DataFrame(columns=['model_name', 'rule', 'no_step', 'learning_rate', 'loss_on_train', 'loss_on_test'])
 
     for model_definition in models_to_evaluate:
@@ -52,16 +52,14 @@ if __name__ == "__main__":
             epochs, train_losses, test_losses = model.fit(train_time, test_time, in_size, no_epoch=no_epochs,
                       no_steps=params['no_step'], method=params['rule'], log_epoch=10)
 
-            model_name = f"autoput-01012017-{type(model).__name__}-{params['rule']}"
+            model_name = f"autoput-01012017-0.2-{type(model).__name__}-{params['rule']}"
             train_losses = [loss.detach().numpy().flatten()[0] for loss in train_losses]
             test_losses = [loss.detach().numpy().flatten()[0] for loss in test_losses]
-            print(train_losses, test_losses)
-
             plt.plot(epochs, train_losses, color='skyblue', linewidth=2, label='train')
             plt.plot(epochs, test_losses, color='darkgreen', linewidth=2, linestyle='dashed', label="test")
             plt.legend()
             plt.savefig(f'img/{model_name}.png')
-
+            plt.clf()
             loss_on_train = model.evaluate(train_time, in_size, method='Trapezoid')
             loss_on_test = model.evaluate(test_time, in_size, method='Trapezoid')
             print(f"Model: {model_name}. Loss on train: {str(loss_on_train.data.numpy())},  "
@@ -76,8 +74,5 @@ if __name__ == "__main__":
             pickle.dump(model, open(model_filepath, 'wb'))
 
     print(evaluation_df)
-    evaluation_df.to_csv('results/jan_autoput_scores_0.1.csv', index=False)
-
-
-
+    evaluation_df.to_csv('results/jan_autoput_scores_0.2.csv', index=False)
 
