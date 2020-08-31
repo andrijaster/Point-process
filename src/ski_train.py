91f426de0aa35338a286dad427e5b8fc8b4360c2
DimitrijeM <dimitrijemilenkovic.dm@gmail.com>
2020-08-31 15:32:03 +0200

    ski train updated

--- src/ski_train.py
+++ src/ski_train.py
@@ -11,7 +11,7 @@ from v2 import BaseTraining
 from v2.GRUPointProcess import GRUPointProcess
 from v2.LSTMPointProcess import LSTMPointProcess
 from v2.RNNPointProcess import RNNPointProcess
-#from v2.FCNPointProcess import FCNPointProcess
+from v2.FCNPointProcess import FCNPointProcess
 
 
 if __name__ == "__main__":
@@ -34,52 +34,60 @@ if __name__ == "__main__":
     out_size = 1
 
     learning_param_map = [
-        {'rule': 'Euler', 'no_step': 10, 'learning_rate': 0.001},
-        {'rule': 'Implicit Euler', 'no_step': 10, 'learning_rate': 0.001},
-        {'rule': 'Trapezoid', 'no_step': 10, 'learning_rate': 0.001},
-        {'rule': 'Simpsons', 'no_step': 10, 'learning_rate': 0.001},
-        {'rule': 'Gaussian_Q', 'no_step': 10, 'learning_rate': 0.001}
+        {'rule': 'Euler', 'no_step': 10, 'learning_rate': 0.01},
+        {'rule': 'Implicit Euler', 'no_step': 10, 'learning_rate': 0.01},
+        {'rule': 'Trapezoid', 'no_step': 10, 'learning_rate': 0.01},
+        {'rule': 'Simpsons', 'no_step': 10, 'learning_rate': 0.01},
+        {'rule': 'Gaussian_Q', 'no_step': 10, 'learning_rate': 0.01}
     ]
     models_to_evaluate = [
-        #{'model': FCNPointProcess(in_size+1, out_size, dropout=0.1), 'learning_param_map': learning_param_map},
-        {'model': GRUPointProcess(in_size+1, out_size, dropout=0.0), 'learning_param_map': learning_param_map},
-        {'model': LSTMPointProcess(in_size+1, out_size, dropout=0.0), 'learning_param_map': learning_param_map},
-        {'model': RNNPointProcess(in_size+1, out_size, dropout=0.0), 'learning_param_map': learning_param_map}
+        {'model': FCNPointProcess, 'learning_param_map': learning_param_map},
+        {'model': GRUPointProcess, 'learning_param_map': learning_param_map},
+        {'model': LSTMPointProcess, 'learning_param_map': learning_param_map},
+        {'model': RNNPointProcess, 'learning_param_map': learning_param_map}
     ]
     print(f'Train size: {str(train_time.shape[1])}, test size: {str(test_time.shape[1])} ('
           f'{round((test_time.shape[1] / (train_time.shape[1] + test_time.shape[1])), 2)} %).')
 
     in_size = 5
     out_size = 1
-    no_epochs = 1000
+    no_epochs = 2000
     evaluation_df = pd.DataFrame(columns=['model_name', 'rule', 'no_step', 'learning_rate', 'training_time',
                                           'loss_on_train', 'loss_on_test'])
 
     for model_definition in models_to_evaluate:
         for params in model_definition['learning_param_map']:
-            model = model_definition['model']
-            model_name = f"ski-kg-{type(model).__name__}-{params['learning_rate']}-{params['rule']}"
+            model = None
+            counter = 0
 
-            print(f"Starting to train a model: {model_name}")
-            t0 = time.time()
+            while not model:
+                counter += 1
+                model = model_definition['model'](in_size+1, out_size, dropout=0.1)
+                model_name = f"zh_main_station-240820-{type(model).__name__}-{params['learning_rate']}-{params['rule']}"
 
-            model = BaseTraining.fit(model, train_time, test_time, in_size, lr=params['learning_rate'],
-                                     no_epoch=no_epochs, no_steps=params['no_step'], method=params['rule'], log_epoch=10,
-                                     figpath=f"{project_dir}/img/{model_name}.png")
+                print(f"{counter}. Starting to train a model: {model_name}")
+                t0 = time.time()
+                model = model_definition['model'](in_size+1, out_size, dropout=0.0)
+                model_name = f"ski-kg-2018-{type(model).__name__}-{params['learning_rate']}-{params['rule']}"
 
-            loss_on_train = BaseTraining.evaluate(model, train_time, in_size, method=params['rule'])
-            loss_on_test = BaseTraining.evaluate(model, test_time, in_size, method='Trapezoid')
-            print(f"Model: {model_name}. Loss on train: {str(loss_on_train.data.numpy().flatten()[0])}, "
-                  f"loss on test: {str(loss_on_test.data.numpy().flatten()[0])}")
-            evaluation_df.loc[len(evaluation_df)] = [type(model).__name__,
-                                                     params['rule'],
-                                                     params['no_step'],
-                                                     params['learning_rate'],
-                                                     str(round(time.time() - t0)),
-                                                     loss_on_train.data.numpy().flatten()[0],
-                                                     loss_on_test.data.numpy().flatten()[0]]
-            model_filepath = f"models/ski/{model_name}.torch"
-            pickle.dump(model, open(model_filepath, 'wb'))
+                t0 = time.time()
+                model = BaseTraining.fit(model, train_time, test_time, in_size, lr=params['learning_rate'],
+                                         no_epoch=no_epochs, no_steps=params['no_step'], method=params['rule'], log_epoch=10,
+                                         figpath=f"{project_dir}/img/{model_name}.png")
+                if model:
+                    loss_on_train = BaseTraining.evaluate(model, train_time, in_size, method=params['rule'])
+                    loss_on_test = BaseTraining.evaluate(model, test_time, in_size, method='Trapezoid')
+                    print(f"Model: {model_name}. Loss on train: {str(loss_on_train.data.numpy().flatten()[0])}, "
+                          f"loss on test: {str(loss_on_test.data.numpy().flatten()[0])}")
+                    evaluation_df.loc[len(evaluation_df)] = [type(model).__name__,
+                                                             params['rule'],
+                                                             params['no_step'],
+                                                             params['learning_rate'],
+                                                             str(round(time.time() - t0)),
+                                                             loss_on_train.data.numpy().flatten()[0],
+                                                             loss_on_test.data.numpy().flatten()[0]]
+                    model_filepath = f"models/ski/{model_name}.torch"
+                    pickle.dump(model, open(model_filepath, 'wb'))
 
     print(evaluation_df)
-    evaluation_df.to_csv(f"results/ski_kg_scores_{str(learning_param_map[0]['learning_rate'])}_0.1.csv", index=False)
+    evaluation_df.to_csv(f"results/ski_kg_2018_scores_{str(learning_param_map[0]['learning_rate'])}_0.4.csv", index=False)
